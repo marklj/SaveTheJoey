c7e5d02d9f8e9b64368cbd98baf51b730a7a2f59
Mark Johnson <marklj@gmail.com>
2011-10-14 15:16:19 -0700

    test.py comments

--- test.py
+++ test.py
@@ -11,12 +11,15 @@ mouse_c = pygame.image.load(ball).convert_alpha()
 
 while True:
     for event in pygame.event.get():
+        #quit the game
         if event.type == QUIT:
             pygame.quit()
             sys.exit
     
+    #render the background
     screen.blit(background, (0,0))
     
+    #make the ball follow the mouse
     x,y = pygame.mouse.get_pos()
     x -= mouse_c.get_width()/2
     y -= mouse_c.get_height()/2
